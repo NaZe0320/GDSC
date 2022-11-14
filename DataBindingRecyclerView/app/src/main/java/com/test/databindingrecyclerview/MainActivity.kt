b5b9bd03f1c169062ca696c6dfc6384594837b48
NaZe0320 <jisong1226@gmail.com>
2022-11-15 01:48:59 +0900

    DataBindingRecyclerViewTest

--- DataBindingRecyclerView/app/src/main/java/com/test/databindingrecyclerview/MainActivity.kt
+++ DataBindingRecyclerView/app/src/main/java/com/test/databindingrecyclerview/MainActivity.kt
@@ -2,10 +2,51 @@ package com.test.databindingrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.test.databindingrecyclerview.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val studentList : ArrayList<Student> = ArrayList()
+    private lateinit var studentAdapter : StudentAdapter
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setAdapter()
+        setListener()
+    }
+
+    private fun setAdapter(){
+        studentAdapter = StudentAdapter(studentList)
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = studentAdapter
+        }
+    }
+
+    private fun setListener() {
+        binding.tvAddItem.setOnClickListener(this)
+        binding.btnSubmit.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            binding.tvAddItem.id -> {
+                studentList.add(Student())
+                Log.d("<PROCESS>", "$studentList")
+                studentAdapter.notifyItemInserted(studentList.size-1)
+            }
+            binding.btnSubmit.id -> {
+                studentList.forEachIndexed { index, student ->
+                    Log.d("<RESULT>", "Name : ${student.name} - Score : ${student.score}")
+                }
+            }
+        }
     }
 }
---
+++ DataBindingRecyclerView/app/src/main/java/com/test/databindingrecyclerview/Student.kt
@@ -0,0 +1,6 @@
+package com.test.databindingrecyclerview
+
+data class Student (
+    var name : String = "",
+    var score : String = ""
+)
